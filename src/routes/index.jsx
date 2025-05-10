08f0ecdade28a53989bda0ed02cca5dbe0dd2805
Hồ Thị Ơn <oho53972@gmail.com>
2025-05-10 21:50:44 +0700

    done in-class-plan UI

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -4,6 +4,8 @@ import Home from '../pages/Home';
 import About from '../pages/About';
 import ClassDetail from '../pages/ClassDetail';
 import SubjectDetail from '../pages/SubjectDetail';
+import StudentJournalPage from '../pages/StudentJournalPage';
+import { Navigate } from 'react-router-dom';
 
 const AppRoutes = () => (
   <Routes>
@@ -11,6 +13,9 @@ const AppRoutes = () => (
     <Route path="/class/:classId" element={<ClassDetail />} />
     <Route path="/subject/:subjectId" element={<SubjectDetail />} />
     <Route path="/about" element={<About />} />
+
+    <Route path="/in-class-plan" element={<Navigate to="/student-journal" />} />
+    <Route path="/student-journal" element={<StudentJournalPage />} />
   </Routes>
 );
 
