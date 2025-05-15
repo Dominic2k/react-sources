53080a96fa547eaeaae3186498f3606ee558b4e4
CED-b2005 <duqingping2k5@gmail.com>
2025-05-15 20:29:16 +0700

    80% complated login React  design

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import {React} from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Home from '../pages/Home';
 import About from '../pages/About';
@@ -8,11 +8,19 @@ import InClassForm from '../pages/InClassForm';
 import ShowInClassForm from '../pages/ShowInClassForm';
 import { Navigate } from 'react-router-dom';
 import StudentProfile from '../pages/Profile/StudentProfile';
+import LoginForm from '../pages/Auth/Login';
+import { useNavigate } from 'react-router-dom';
 
 const AppRoutes = () => (
+
   <Routes>
     <Route path='/studentProfile' element={<StudentProfile/>}/>
+
     <Route path="/" element={<Home />} />
+    <Route path="/home" element={<Home />} />
+
+    <Route path="/login" element={<LoginForm />} />
+
     <Route path="/subject/:subjectId" element={<SubjectDetail />} />
     <Route path="/about" element={<About />} />
     <Route path="/self-study/:className" element={<SelfStudyPlan />} />
@@ -20,7 +28,8 @@ const AppRoutes = () => (
     <Route path="/in-class-form" element={<InClassForm />} />
 
     <Route path="/showinclassform" element={<ShowInClassForm/>} />
-  </Routes>
-);
 
+  </Routes>
+  
+);
 export default AppRoutes;
